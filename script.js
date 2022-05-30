7e14c5a29f35bf75000e16044ec32630759ae792
Mariam-Gug <89607619+Mariam-Gug@users.noreply.github.com>
2022-05-30 23:51:18 +0400

    finished validation

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ document.getElementById('registration').addEventListener('submit', function(even
         errors.username = 'Please enter at least 4 symbols';
     };
 
+    // mail
+    let mail = document.getElementById('email').value;
+    if (mail == '') {
+        errors.mail = 'Please enter your email';
+    }
+
     // password
     let password = document.getElementById('password').value;
     let password2 = document.getElementById('password2').value;
@@ -19,13 +25,13 @@ document.getElementById('registration').addEventListener('submit', function(even
     if(password != password2 || password == '') {
         errors.password = 'Please enter at least 6 symbols';
         errors.password2 = 'Passwords do not match';
-    }
+    };
 
     // checkbox
-    let agree = document.getElementById('check-agree').checked;
-    if(agree == false) {
+    let agree = document.getElementById('agree').checked;
+    if(!agree) {
         errors.agree = 'You have not agreed to the terms and conditions';
-    }
+    };
 
     // radio
     let age = false;
@@ -57,4 +63,54 @@ document.getElementById('registration').addEventListener('submit', function(even
             errorSpan.textContent = errors[item];
         }
     }
+
+    if (Object.keys(errors).length == 0) {
+        form.submit();
+    }
 });
+
+
+    // show/hide password
+    let passwordShow = document.getElementById('password');
+    let toggleIconEye = document.getElementById('toggleIcon');
+    let passwordShow2 = document.getElementById('password2');
+    let toggleIconEye2 = document.getElementById('toggleIcon2');
+
+    showHidePassword1 = () => {
+        if (passwordShow.type =='password') {
+            passwordShow.setAttribute('type', 'text');
+            toggleIconEye.classList.add('fa-eye-slash');
+        } else {
+            toggleIconEye.classList.remove('fa-eye-slash');
+            passwordShow.setAttribute('type', 'password');
+        }
+    };
+
+    showHidePassword2 = () => {
+        if (passwordShow2.type == 'password') {
+            passwordShow2.setAttribute('type', 'text')
+            toggleIconEye2.classList.add('fa-eye-slash');
+        } else {
+            passwordShow2.setAttribute('type', 'password');
+            toggleIconEye2.classList.remove('fa-eye-slash');
+        }
+    }
+
+    toggleIconEye.addEventListener('click', showHidePassword1);
+    toggleIconEye2.addEventListener('click', showHidePassword2);
+
+
+
+    // mail validation
+    function validation() {
+        let emailField = document.getElementById('email').value;
+        let emailSpan = document.getElementById('error_mail');
+        let emailStructure = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (emailField.match(emailStructure)) {
+            emailSpan.innerHTML = 'Your email is valid';
+            emailSpan.style.color = 'MediumSpringGreen';
+        } else {
+            emailSpan.innerHTML = 'Your email is invalid';
+        }
+    }
